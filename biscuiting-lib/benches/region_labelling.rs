e54338a70c1d9689f0e813c9e6f1c8a7c596c43a
Mike Moran <mike@houseofmoran.com>
2020-03-01 22:31:43 +0000

    introduce randomised images

--- biscuiting-lib/benches/region_labelling.rs
+++ biscuiting-lib/benches/region_labelling.rs
@@ -1,47 +1,65 @@
 #[macro_use]
 extern crate imageproc;
+extern crate rand;
 
 use image::GenericImage;
 use image::ImageBuffer;
 use image::Luma;
-use imageproc::utils::gray_bench_image;
 
 use biscuiting_lib::region_labelling::find_contours;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn tiny_example_benchmark(c: &mut Criterion) {
-    let mut image = gray_image!(type: u32,
-        0,   0, 100, 100;
-        0,   0, 100, 100;
-        0, 255, 255, 0;
-        0, 255, 255, 0);
+    let mut image = random_test_image(3, 3, 0u64);
 
     let background_color = Luma([0u32; 1]);
 
     let sub_image = image.sub_image(0, 0, image.width(), image.height());
-    let contours = c.bench_function("tiny_example_benchmark", |b| {
-        b.iter(|| find_contours(background_color, &sub_image))
+    c.bench_function("tiny_example_benchmark", |b| {
+        b.iter(|| find_contours(black_box(background_color), &sub_image))
+    });
+}
+
+fn medium_example_benchmark(c: &mut Criterion) {
+    let mut image = random_test_image(100, 100, 0u64);
+
+    let background_color = Luma([0u32; 1]);
+
+    let sub_image = image.sub_image(0, 0, image.width(), image.height());
+    c.bench_function("medium_example_benchmark", |b| {
+        b.iter(|| find_contours(black_box(background_color), &sub_image))
+    });
+}
+
+fn large_example_benchmark(c: &mut Criterion) {
+    let mut image = random_test_image(1000, 1000, 0u64);
+
+    let background_color = Luma([0u32; 1]);
+
+    let sub_image = image.sub_image(0, 0, image.width(), image.height());
+    c.bench_function("medium_example_benchmark", |b| {
+        b.iter(|| find_contours(black_box(background_color), &sub_image))
     });
 }
 
 type TestImage = ImageBuffer<Luma<u32>, Vec<u32>>;
 
-fn large_example_benchmark(c: &mut Criterion) {
-    let mut image = TestImage::new(1000, 1000);
+fn random_test_image(width: u32, height: u32, seed: u64) -> TestImage {
+    use rand::prelude::*;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+    let mut rng: StdRng = SeedableRng::seed_from_u64(seed);
+
+    let mut image = TestImage::new(width, height);
     for y in 0..image.height() {
         for x in 0..image.width() {
-            let intensity = (x % 7 + y % 6) as u32;
+            let intensity = rng.gen::<u32>();
             image.put_pixel(x, y, Luma::<u32>([intensity; 1]));
         }
     }
 
-    let background_color = Luma([0u32; 1]);
-
-    let sub_image = image.sub_image(0, 0, image.width(), image.height());
-    let contours = c.bench_function("large_example_benchmark", |b| {
-        b.iter(|| find_contours(background_color, &sub_image))
-    });
+    image
 }
 
-criterion_group!(benches, tiny_example_benchmark, large_example_benchmark);
+criterion_group!(benches, tiny_example_benchmark, medium_example_benchmark);
 criterion_main!(benches);
