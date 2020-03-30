e9790ad6f6d8bdfa7f67cca5c40efb2fd8e402f0
Mike Moran <mike@houseofmoran.com>
2020-03-30 22:48:37 +0100

    add a rigid body

--- spikes/nphysics2d-example/src/main.rs
+++ spikes/nphysics2d-example/src/main.rs
@@ -3,7 +3,7 @@ extern crate nalgebra as na;
 use na::Vector2;
 use nphysics2d::force_generator::DefaultForceGeneratorSet;
 use nphysics2d::joint::DefaultJointConstraintSet;
-use nphysics2d::object::{DefaultBodySet, DefaultColliderSet};
+use nphysics2d::object::{DefaultBodySet, DefaultColliderSet, RigidBodyDesc};
 use nphysics2d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
 
 fn main() {
@@ -11,10 +11,16 @@ fn main() {
     let mut mechanical_world = DefaultMechanicalWorld::new(gravity);
     let mut geometrical_world = DefaultGeometricalWorld::new();
 
-    let mut bodies = DefaultBodySet::new();
     let mut colliders = DefaultColliderSet::new();
     let mut joint_constraints = DefaultJointConstraintSet::new();
     let mut force_generators = DefaultForceGeneratorSet::new();
+
+    let mut bodies = DefaultBodySet::new();
+    let rigid_body = RigidBodyDesc::new()
+        .translation(Vector2::y() * 5.0)
+        .mass(1.2)
+        .build();
+    bodies.insert(rigid_body);
 
     loop {
         // Run the simulation.
