d8e8631f3e41ff0290d5f30fc5fcec0de7e936a2
Mike Moran <mike@houseofmoran.com>
2020-04-18 23:11:33 +0100

    Spiking on Plus Codes and Mapbox Turf
    - base olc-bounds spike on map-registration
    - use mapbox turf to define reticule based on a fixed radius
    - add scale control (shows metric distance on map)
    - add summary of spec + OLC
    - try fitBounds, change sizes
    - add zoom control
    - set center of display based on center of code-area
    - load location based on plus-code
    - add Edinburgh

---
+++ spikes/olc-bounds/src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./App.scss";
+import { MapView } from "./MapView";
+import dotenv from "dotenv";
+
+import { MapBoxContextProviderFromEnvironment } from "./MapBoxContext";
+
+dotenv.config();
+
+const cities = [
+  {
+    name: "San Francisco",
+    location: {
+      latitude: 37.774929,
+      longitude: -122.419418,
+      plus_code: "849VQHFJ+X6",
+    },
+  },
+  {
+    name: "Jerusalem",
+    location: {
+      latitude: 31.768318,
+      longitude: 35.213711,
+      plus_code: "8G3QQ697+8F",
+    },
+  },
+  {
+    name: "Berlin",
+    location: {
+      latitude: 52.520008,
+      longitude: 13.404954,
+      plus_code: "9F4MGCC3+2X",
+    },
+  },
+  {
+    name: "London",
+    location: {
+      latitude: 51.507351,
+      longitude: -0.127758,
+      plus_code: "9C3XGV4C+WV",
+    },
+  },
+  {
+    name: "Edinburgh",
+    location: {
+      plus_code: "9C7RXR36+8M",
+    },
+  },
+];
+
+function App() {
+  return (
+    <MapBoxContextProviderFromEnvironment>
+      <div>
+        <MapView city={cities[0]} />
+      </div>
+    </MapBoxContextProviderFromEnvironment>
+  );
+}
+
+export default App;
